435ae5a83c1a5542d9279a42afd3d5d5de47bcc7
cj-kid <chanderjindal2@gmail.com>
2021-09-03 10:01:14 +0530

    test commmit

---
+++ game_config.py
@@ -0,0 +1,14 @@
+import os
+
+IMAGE_SIZE = 32
+SCREEN_SIZE = 324
+NUM_TILES_SIDE = 10
+NUM_TILES_TOTAL = 100
+MINES = 10
+MARGIN = 2
+
+ASSET_DIR = 'assets'
+ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png']
+#assert len(ASSET_FILES) == 13
+# test 2git commit
+
